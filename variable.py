e773c24ba1fb7b0c8a800922709072c88ab9a279
Nicolas8713 <171wita09@o365.htl-leoben.at>
2018-03-12 14:39:24 +0100

    variable

--- variable.py
+++ variable.py
@@ -1,1 +1,21 @@
 #Variabe
+'''
+a =10
+b = 20
+summe = a+b
+
+print(" Summe von " , a , "+" ,b , " = " ,summe)
+
+aktion = input("Welche aktion willst du ? (+/-/*/:)")
+print("Aktion ", aktion, "wird ausgeführt")
+
+if (aktion == "*")
+    print("Mutliplikation ausgewählt")
+zahl = input("Welche Zahl wählst du ?")
+zahl = int(zahl)
+
+zahl2 = int("23")
+zahlA = "123"
+zahlB="456"
+print(zahlA + zahlB)
+'''
